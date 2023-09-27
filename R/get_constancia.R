d9d194c232a689bb409b1c6f2ffb392ffe0ce6a7
Samuel Calderon <samuel.calderon@uarm.pe>
2023-09-27 12:18:41 -0500

    add timeout parameter

--- R/get_constancia.R
+++ R/get_constancia.R
@@ -5,6 +5,7 @@
 #'
 #' @param n_dni character. Un número de DNI otorgado por RENIEC Perú.
 #' @param nombre character. Apellidos y Nombres de una persona natural.
+#' @inheritParams httr2::req_timeout
 #'
 #' @return character. Información sobre la persona buscada, en formato JSON.
 #' @export
@@ -12,7 +13,7 @@
 #' @examples
 #' get_constancia(n_dni = "08192557") # DNI de Superintendente
 #' get_constancia(nombre = "ZEGARRA ROJAS OSWALDO DELFIN") # Nombre de Superintendente
-get_constancia <- function(n_dni = NULL, nombre = NULL) {
+get_constancia <- function(n_dni = NULL, nombre = NULL, timeout = 10L) {
     httr2::request("https://constancias.sunedu.gob.pe/consulta") |>
         httr2::req_body_form(
             doc=n_dni,
@@ -23,6 +24,7 @@ get_constancia <- function(n_dni = NULL, nombre = NULL) {
             # captcha=""
         ) |>
         httr2::req_retry(max_seconds = 15) |>
+        httr2::req_timeout(seconds = timeout) |>
         httr2::req_perform() |>
         httr2::resp_body_string() |>
         jsonlite::parse_json()
